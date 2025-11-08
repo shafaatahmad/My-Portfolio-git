c71367d1110d0c17a614dc95fe4c867d03bafd53
Shafaat Ahmad <shafaatrangrezz@gmail.com>
2025-11-08 23:10:22 +0530

    as
    sas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { Main } from "./main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
